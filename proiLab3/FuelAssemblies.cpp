af6a55bc36b213673c68d7f5e88c7a2c24a33995
Wojtek M <wojciech.moczydlowski98@gmail.com>
2019-01-13 12:31:32 +0100

    with FuelAssemblies

--- proiLab3/FuelAssemblies.cpp
+++ proiLab3/FuelAssemblies.cpp
@@ -4,9 +4,35 @@
 
 FuelAssemblies::FuelAssemblies()
 {
+	spaceForFuel = 13000;
+	supplyOfFuel = 0;
 }
 
 
 FuelAssemblies::~FuelAssemblies()
 {
 }
+
+
+void FuelAssemblies::setSupplyOfFuel(int fuel) {
+	
+	if (fuel > spaceForFuel) supplyOfFuel = spaceForFuel;
+	else supplyOfFuel = fuel;
+};
+
+void FuelAssemblies::setSpaceOfFuel(int space) {
+
+	spaceForFuel = space;
+};
+
+void FuelAssemblies::setRateOfCombustion(int rate) {
+	rateOfCombustion = rate;
+};
+
+bool FuelAssemblies::combastFuel() {
+	if (supplyOfFuel > rateOfCombustion) {
+		supplyOfFuel -= rateOfCombustion;
+		return true;
+	}
+	else return false;	
+};
--- proiLab3/FuelAssemblies.h
+++ proiLab3/FuelAssemblies.h
@@ -5,5 +5,20 @@ class FuelAssemblies : public NuclearPlantElement
 public:
 	FuelAssemblies();
 	~FuelAssemblies();
+
+	void setSupplyOfFuel(int fuel);
+	void setSpaceOfFuel(int space);
+	void setRateOfCombustion(int rate);
+
+	int getSupplyOfFuel() { return supplyOfFuel; }
+	int getSpaceForFuel() { return spaceForFuel; }
+	int getRateOfCombustion() { return rateOfCombustion; }
+
+	bool combastFuel();
+
+private:
+	int spaceForFuel;
+	int supplyOfFuel;
+	int rateOfCombustion;
 };
 
